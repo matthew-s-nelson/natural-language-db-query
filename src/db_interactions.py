fba5d2a829e70d518027799856b2f65ea07bac39
Matthew Nelson <matthewbuzz@gmail.com>
2025-09-27 19:46:52 -0600

    Add a function to get the DB's schema

--- src/db_interactions.py
+++ src/db_interactions.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-def initialize_db():
-    db_file_path = '../kitchen.db'
-    sql_file_path = '../kitchendbsqlite.sql'
+db_file_path = '../kitchen.db'
+sql_file_path = '../kitchendbsqlite.sql'
 
+def initialize_db():
     with open(sql_file_path, 'r') as sql_file:
         sql_script = sql_file.read()
 
@@ -25,6 +25,35 @@ def initialize_db():
             
             cursor.executescript(sql_script)
             conn.commit()
+            cursor.close()
         print('Kitchen sqlite DB formed.')
     except Exception as e:
         print(f'Kitchen sqlite DB not formed: {e}')
+
+def get_db_schema():
+    with sqlite3.connect(db_file_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+
+        schema = {}
+        for table_name in tables:
+            table = table_name[0]
+            cursor.execute(f"PRAGMA table_info({table});")
+            columns = cursor.fetchall()
+            schema[table] = [col[1] for col in columns]  # col[1] is the column name
+        cursor.close()
+
+    return schema
+
+def execute_query(query: str):
+    try:
+        with sqlite3.connect(db_file_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            results = cursor.fetchall()
+            cursor.close()
+    except Exception as e:
+        print(f"Error executing query: {e}")
+        return []
+    return results
